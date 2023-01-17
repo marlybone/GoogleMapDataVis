74459ac82239f647852849e666528f5ca334cacc
M <41431476+marlybone@users.noreply.github.com>
2023-01-17 21:29:22 +0000

    some house-keeping, removing some functions that are legacy.

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 var APIKEY = 'AIzaSyC75kqs_RD694ILnPBt0cOAsyzQwpSBfaU';
-const myLngLat = {lat: 51.4931, lng: -0.118092};
+const myLngLat = { lat: 51.4931, lng: -0.118092 };
 var mapStyle = [{
-          'featureType': 'all',
-          'elementType': 'all',
-          'stylers': [{'visibility': 'off'}]
-        }, {
-          'featureType': 'landscape',
-          'elementType': 'geometry',
-          'stylers': [{'visibility': 'on'}, {'color': '#fcfcfc'}]
-        }, {
-          'featureType': 'water',
-          'elementType': 'labels',
-          'stylers': [{'visibility': 'off'}]
-        }, {
-          'featureType': 'water',
-          'elementType': 'geometry',
-          'stylers': [{'visibility': 'on'}, {'hue': '#5f94ff'}, {'lightness': 60}]
-        }];
+  'featureType': 'all',
+  'elementType': 'all',
+  'stylers': [{ 'visibility': 'off' }]
+}, {
+  'featureType': 'landscape',
+  'elementType': 'geometry',
+  'stylers': [{ 'visibility': 'on' }, { 'color': '#fcfcfc' }]
+}, {
+  'featureType': 'water',
+  'elementType': 'labels',
+  'stylers': [{ 'visibility': 'off' }]
+}, {
+  'featureType': 'water',
+  'elementType': 'geometry',
+  'stylers': [{ 'visibility': 'on' }, { 'hue': '#5f94ff' }, { 'lightness': 60 }]
+}];
 
 let min = Number.MAX_VALUE;
 let max = -Number.MAX_VALUE;
@@ -26,7 +26,7 @@ let map;
 let admin;
 
 /* variables to contribute to marker and city location for API retrieval*/
-var lat = ''; 
+var lat = '';
 var lng = '';
 var place = '';
 var cityInput = '';
@@ -52,7 +52,7 @@ let value;
 let costs;
 
 
-/* PlacesSearch function*/ 
+/* PlacesSearch function*/
 var mapDiv = document.getElementById('map');
 var mapOptions = {
   center: myLngLat,
@@ -65,41 +65,41 @@ let places = [];
 
 /* API key for RapidAPI*/
 const options = {
-	method: 'GET',
-	headers: {
+  method: 'GET',
+  headers: {
     'X-RapidAPI-Key': '465cd1dd34msh19e6aab3eb40ec2p172d7djsn2291a31a6271',
-		'X-RapidAPI-Host': 'cost-of-living-and-prices.p.rapidapi.com'
-	}
+    'X-RapidAPI-Host': 'cost-of-living-and-prices.p.rapidapi.com'
+  }
 };
-     var mapOptions = {
-     center: myLngLat,
-     zoom: 5,
-     styles: mapStyle,
-   };
+var mapOptions = {
+  center: myLngLat,
+  zoom: 5,
+  styles: mapStyle,
+};
 
 
 /* Event Listeners */
-  const select = document.getElementById('census-variable')
-  let selected = select.value;
- select.addEventListener('change', () => {
-      clearData();
-      selected = select.value;
-      loadTheData();
-    })
+const select = document.getElementById('census-variable')
+let selected = select.value;
+select.addEventListener('change', () => {
+  clearData();
+  selected = select.value;
+  loadTheData();
+})
 
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), mapOptions)
-    var marker = new google.maps.Marker({
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), mapOptions)
+  var marker = new google.maps.Marker({
     position: myLngLat,
-    map:map,
+    map: map,
     draggable: false
   });
 
-    map.data.setStyle(styleFeature);
-    map.data.addListener("mouseover", mouseInToRegion);
-    map.data.addListener("mouseout", mouseOutOfRegion);
+  map.data.setStyle(styleFeature);
+  map.data.addListener("mouseover", mouseInToRegion);
+  map.data.addListener("mouseout", mouseOutOfRegion);
 
-    google.maps.event.addListenerOnce(map.data, "addfeature", () => {
+  google.maps.event.addListenerOnce(map.data, "addfeature", () => {
     google.maps.event.trigger(
       document.getElementById("census-variable"),
       "change"
@@ -109,7 +109,7 @@ const options = {
   const input = document.getElementById('country-name');
   const autoComplete = new google.maps.places.Autocomplete(input, cityOptions);
   autoComplete.bindTo("bounds", map);
-  
+
   autoComplete.addListener('place_changed', () => {
     var place = autoComplete.getPlace();
     if (place.geometry) {
@@ -120,92 +120,78 @@ const options = {
       latLong = ({ lat, lng });
     }
   });
- 
-async function searchPlaces() {
+
+  async function searchPlaces() {
     const service = new google.maps.places.PlacesService(map);
     const { results, status } = await new Promise(resolve => service.nearbySearch({
-        location: latLong,
-        radius: 5000,
-        type: 'tourist_attraction'
+      location: latLong,
+      radius: 5000,
+      type: 'tourist_attraction'
     }, (results, status) => resolve({ results, status })));
 
     if (status !== google.maps.places.PlacesServiceStatus.OK) return;
 
     const details = await Promise.all(results.map(result => new Promise(resolve => service.getDetails({
-        placeId: result.place_id,
-        fields: ['name', 'formatted_address', 'geometry', 'rating', 'review', 'photos'],
-        language: 'en'
+      placeId: result.place_id,
+      fields: ['name', 'formatted_address', 'geometry', 'rating', 'review', 'photos'],
+      language: 'en'
     }, (place, status) => resolve({ place, status })))));
     places = details.filter(({ status }) => status === google.maps.places.PlacesServiceStatus.OK).map(({ place }) => place);
-    if(places.some(place=> place.photos)){
-        let photoUrls = places.map(place => place.photos.map((photo) => {
+    if (places.some(place => place.photos)) {
+      let photoUrls = places.map(place => place.photos.map((photo) => {
         let url = photo.getUrl({ maxWidth: 550, maxHeight: 550 });
         return url;
-        })).flat();
+      })).flat();
       let updatedPlaces = places.map((place) => {
-  if(place.photos) {
-    let updatedPhotos = place.photos.map(photo => {
-        let url = photo.getUrl({ maxWidth: 550, maxHeight: 550 });
-        return {...photo, html_attributions: url}
-    });
-    return {...place, photos: updatedPhotos}
-  }
-  return place;
-})
-sessionStorage.setItem('updatedPlaces', JSON.stringify(updatedPlaces));
+        if (place.photos) {
+          let updatedPhotos = place.photos.map(photo => {
+            let url = photo.getUrl({ maxWidth: 550, maxHeight: 550 });
+            return { ...photo, html_attributions: url }
+          });
+          return { ...place, photos: updatedPhotos }
+        }
+        return place;
+      })
+      sessionStorage.setItem('updatedPlaces', JSON.stringify(updatedPlaces));
     }
-  window.open('tourism.html')
-}
+    window.open('tourism.html')
+  }
 
   var element = document.getElementById('button');
   element.addEventListener('click', function() {
-  citySearch();
-  searchPlaces();
-});
-    loadMapShapes();
+    citySearch();
+    searchPlaces();
+  });
+  loadMapShapes();
 }
 
-async function loadMapShapes() { map.data.loadGeoJson('https://s3.amazonaws.com/rawstore.datahub.io/23f420f929e0e09c39d916b8aaa166fb.geojson', { idPropertyName: "ISO_A3" });
+async function loadMapShapes() {
+  map.data.loadGeoJson('https://s3.amazonaws.com/rawstore.datahub.io/23f420f929e0e09c39d916b8aaa166fb.geojson', { idPropertyName: "ISO_A3" });
 
-   map.data.addListener('click', function(e) {
+  map.data.addListener('click', function(e) {
     searchLocation = (e.feature.getProperty('ISO_A3').slice(0, 2));
     fetchMapOverlapData();
   });
-    map.data.addListener('click', function(e) {
+  map.data.addListener('click', function(e) {
     lat = e.latLng.lng();
     lng = e.latLng.lat();
-});
-}
-
-/* function to get the value of the city and then only get the first word of the city */
-function citySearch() {
- searchInput = document.getElementById('country-name').value;
-  cityInput = searchInput.match(/\b\w+/);
-    fetchCityData();
-}
-
-/* Another API call to geoDB to get the data for the specific city */
-function fetchCityData() {
-  fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${cityInput}&types=CITY`, options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+  });
 }
 
 function createBoxes(data) {
   const container = document.querySelector("#container");
-    data.forEach(obj => {
-      let div = document.createElement('div');
-        let divtwo = document.createElement('div')
+  data.forEach(obj => {
+    let div = document.createElement('div');
+    let divtwo = document.createElement('div')
     div.classList.add('box');
     divtwo.classList.add('box-two');
-      if(obj.photos && obj.photos.length > 0) {
-        let randomIndex = Math.floor(Math.random()*obj.photos.length);
-          let randomPhoto = obj.photos[randomIndex];
-    div.innerHTML = `
+    if (obj.photos && obj.photos.length > 0) {
+      let randomIndex = Math.floor(Math.random() * obj.photos.length);
+      let randomPhoto = obj.photos[randomIndex];
+      div.innerHTML = `
       <img src=${randomPhoto.html_attributions}/>
       `;
-    divtwo.innerHTML = `
+      divtwo.innerHTML = `
     <div class='obj-name'>${obj.name}</div>
      ${obj.rating ? `
     <div class='rating'>
@@ -219,18 +205,18 @@ function createBoxes(data) {
             <ul class="slider-container"> 
               ${obj.reviews ? obj.reviews.map(review => `
               <li>
-                 ${review.author_name ?  
-                  `<div class='review' style="max-width: 100%;height:auto;">${review.author_name}</div>` : ''}
+                 ${review.author_name ?
+          `<div class='review' style="max-width: 100%;height:auto;">${review.author_name}</div>` : ''}
                   ${review.text ? `<p>${review.text}</p>` : ''}
-                  ${review.relative_time_description ? 
-                  `<h6>${review.relative_time_description}</h6>` : ''}
+                  ${review.relative_time_description ?
+          `<h6>${review.relative_time_description}</h6>` : ''}
               </li> `).join('') : ''}
             </ul>
               <button type="button" class="slider-nav"></button>
               <button type="button" class="slider-nav slider-nav-next"></button>
      `;
-          div.appendChild(divtwo)
-          container.appendChild(div);
+      div.appendChild(divtwo)
+      container.appendChild(div);
     }
   })
 }
@@ -247,59 +233,57 @@ function livingCost() {
   countryName = value.match(/\b\w+$/)[0];
 
   fetch(`https://cost-of-living-and-prices.p.rapidapi.com/prices?city_name=${cityName}&country_name=${countryName}`, options)
-  .then(res => res.json())
-  .then(data => {
-    cost = Object.assign({}, data);
-    console.log(cost)
-  });
+    .then(res => res.json())
+    .then(data => {
+      cost = Object.assign({}, data);
+      console.log(cost)
+    });
 }
 
 function loadTheData() {
-    fetch('data.csv')
-      .then(res => res.text())
-      .then(data => {
-        const rows = data.split(/\r?\n/).map(row => {
+  fetch('data.csv')
+    .then(res => res.text())
+    .then(data => {
+      const rows = data.split(/\r?\n/).map(row => {
         if (row.includes('"')) {
-        return row.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
-      }
+          return row.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+        }
         else {
-        return row.split(',');
-  }      
-});
-        console.log(rows)
-        const rowss = rows.filter(rows => rows.includes(selected));
-        countryData = rowss.map(row => ({ index11: parseFloat(Number(row[12]).toFixed(2)), index0: row[1]})).filter(element => !isNaN(element.index11)); 
+          return row.split(',');
+        }
+      });
+      const rowss = rows.filter(rows => rows.includes(selected));
+      countryData = rowss.map(row => ({ index11: parseFloat(Number(row[12]).toFixed(2)), index0: row[1] })).filter(element => !isNaN(element.index11));
 
-        countryData.forEach((row) => {
-          if(row.index11 < min) {
-            min = Math.floor(row.index11);
-          }
-          if(row.index11 > max) {
-            max = Math.ceil(row.index11);
-          }
+      countryData.forEach((row) => {
+        if (row.index11 < min) {
+          min = Math.floor(row.index11);
+        }
+        if (row.index11 > max) {
+          max = Math.ceil(row.index11);
+        }
         document.getElementById('census-min').textContent =
           min.toLocaleString();
         document.getElementById('census-max').textContent =
           max.toLocaleString();
-       const admin = map.data.getFeatureById(row.index0)
+        const admin = map.data.getFeatureById(row.index0)
         if (admin) {
           admin.setProperty('census_variable', row.index11)
-          console.log(admin)
         }
-        });
       });
-  }
+    });
+}
 
 /* function to clear the date and return values back to default*/
-function clearData(){
-      min = Number.MAX_VALUE;
-      max = -Number.MAX_VALUE;
+function clearData() {
+  min = Number.MAX_VALUE;
+  max = -Number.MAX_VALUE;
 
-      map.data.forEach((row) =>{
-        row.setProperty('census_variable', undefined);       
-      });
-      document.getElementById('data-box').style.display = 'none';
-      document.getElementById("data-caret").style.display = "none";
+  map.data.forEach((row) => {
+    row.setProperty('census_variable', undefined);
+  });
+  document.getElementById('data-box').style.display = 'none';
+  document.getElementById("data-caret").style.display = "none";
 }
 
 /*function to connect colors to data and styles*/
@@ -348,7 +332,7 @@ function mouseInToRegion(e) {
       (max - min)) *
     100;
 
- document.getElementById("data-label").textContent =
+  document.getElementById("data-label").textContent =
     e.feature.getProperty("ADMIN");
   document.getElementById("data-value").textContent = e.feature
     .getProperty("census_variable")
@@ -362,21 +346,14 @@ function mouseInToRegion(e) {
 function mouseOutOfRegion(e) {
   e.feature.setProperty("admin", "normal");
 }
-  
+
 
 /*
-
-cost of living is selected and types in city name and search button will save values on the inputted city - 
-
-then I will need to get the city name and coutry name and put them in different variables
-
-use these two variables to make the get request
-
-present the data on the tourism.html much the same as the tourist attractions
 -------------------------------------------------------------------------------
-to add - 
+to add -
 *button to increase distance to search tourist attractions - 2.5k - 5k - 10k - 15k
-* add a table with cost of living data
-*css improvements and look at efficiency
-
+* add a table with cost of living data from the cost of living api
+*css improvements
+*SEO improvements
+*efficiency improvements
 */
